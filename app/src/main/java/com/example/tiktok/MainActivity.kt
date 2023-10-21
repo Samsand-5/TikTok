37a03133018557745b5020f84939353725914be1
coder <soumyodipsutra@gmail.com>
2023-10-21 20:15:36 +0530

    bottom navigation menu completed successfully

--- app/src/main/java/com/example/tiktok/MainActivity.kt
+++ app/src/main/java/com/example/tiktok/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.tiktok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiktok.databinding.ActivityMainBinding
+import com.example.tiktok.util.UiUtil
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavBar.setOnItemSelectedListener {menuItem->
+            when(menuItem.itemId){
+                R.id.bottom_menu_home->{
+                    UiUtil.showToast(this,"Home")
+                }
+                R.id.bottom_menu_add_video->{
+                    UiUtil.showToast(this,"Add Video")
+                }
+                R.id.bottom_menu_profile->{
+                    UiUtil.showToast(this,"Profile")
+                }
+            }
+            false
+        }
     }
 }
