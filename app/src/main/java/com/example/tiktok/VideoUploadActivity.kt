7c708c8cb0b42cfdaae7a7452749eaea4b0c3f62
coder <soumyodipsutra@gmail.com>
2023-10-26 09:54:24 +0530

    creating video upload activity

--- app/src/main/java/com/example/tiktok/VideoUploadActivity.kt
+++ app/src/main/java/com/example/tiktok/VideoUploadActivity.kt
@@ -1,11 +1,20 @@
 package com.example.tiktok
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import com.example.tiktok.databinding.ActivityVideoUploadBinding
 
 class VideoUploadActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityVideoUploadBinding
+    private var selectedVideoUri: Uri?= null
+    lateinit var videoLauncher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_video_upload)
+        binding = ActivityVideoUploadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
