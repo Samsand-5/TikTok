0edd007d0d2ee8e3979975f52fcb854acb0c6af0
coder <soumyodipsutra@gmail.com>
2023-11-03 22:37:31 +0530

    making inner class for VideoViewHolder in Class VideoListAdapter.kt

--- app/src/main/java/com/example/tiktok/adapter/VideoListAdapter.kt
+++ app/src/main/java/com/example/tiktok/adapter/VideoListAdapter.kt
@@ -1,4 +1,9 @@
 package com.example.tiktok.adapter
 
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tiktok.databinding.VideoItemRowBinding
+
 class VideoListAdapter {
+
+    inner class VideoViewHolder(binding : VideoItemRowBinding) : RecyclerView.ViewHolder(binding.root)
 }
