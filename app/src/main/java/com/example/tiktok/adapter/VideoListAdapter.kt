98022d22d8275eb2315b9805a75b6e9be7c33e59
coder <soumyodipsutra@gmail.com>
2023-11-03 22:44:22 +0530

    making inner class for VideoViewHolder in Class VideoListAdapter.kt

--- app/src/main/java/com/example/tiktok/adapter/VideoListAdapter.kt
+++ app/src/main/java/com/example/tiktok/adapter/VideoListAdapter.kt
@@ -2,8 +2,20 @@ package com.example.tiktok.adapter
 
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tiktok.databinding.VideoItemRowBinding
+import com.example.tiktok.model.VideoModel
 
 class VideoListAdapter {
 
-    inner class VideoViewHolder(binding : VideoItemRowBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class VideoViewHolder(private var binding : VideoItemRowBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bindVideo(videoModel: VideoModel) {
+            //bind video
+            binding.videoView.apply {
+                setVideoPath(videoModel.url)
+                setOnPreparedListener {
+
+                }
+            }
+        }
+    }
 }
