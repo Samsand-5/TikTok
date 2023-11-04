ba969bc11b7afb00ec93e5241543423f210cef0a
coder <soumyodipsutra@gmail.com>
2023-11-04 13:54:16 +0530

    passing adapter in mainactivity to watch video

--- app/src/main/java/com/example/tiktok/MainActivity.kt
+++ app/src/main/java/com/example/tiktok/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.tiktok
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiktok.adapter.VideoListAdapter
 import com.example.tiktok.databinding.ActivityMainBinding
 import com.example.tiktok.util.UiUtil
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var adapter: VideoListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
