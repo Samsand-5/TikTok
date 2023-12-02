06d05a43db85502ac148b39a79fec64f05afd82d
coder <soumyodipsutra@gmail.com>
2023-12-02 10:16:23 +0530

    getting current userId from firebase

--- app/src/main/java/com/example/tiktok/ProfileActivity.kt
+++ app/src/main/java/com/example/tiktok/ProfileActivity.kt
@@ -3,14 +3,19 @@ package com.example.tiktok
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tiktok.databinding.ActivityProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileActivity : AppCompatActivity() {
     lateinit var binding: ActivityProfileBinding
     lateinit var profileUserId: String
+    lateinit var currentUserId: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        profileUserId = intent.getStringExtra("profile_user_id").toString()
+        //getting profile userId from MainActivity.kt
+        profileUserId = intent.getStringExtra("profile_user_id")!!
+        // getting current userId from Firebase
+        currentUserId = FirebaseAuth.getInstance().currentUser?.uid!!
     }
 }
