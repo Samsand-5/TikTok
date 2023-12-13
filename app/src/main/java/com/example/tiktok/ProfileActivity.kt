7e7eaa8e48d7a63b0bac5de1dce67d4afbe5e19e
coder <soumyodipsutra@gmail.com>
2023-12-13 22:44:12 +0530

    photolauncher working procvess

--- app/src/main/java/com/example/tiktok/ProfileActivity.kt
+++ app/src/main/java/com/example/tiktok/ProfileActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.tiktok.databinding.ActivityProfileBinding
@@ -19,10 +21,19 @@ class ProfileActivity : AppCompatActivity() {
     lateinit var profileUserId: String
     lateinit var currentUserId: String
     lateinit var profileUserModel: UserModel
+    lateinit var photoLauncher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        photoLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result->
+            if(result.resultCode == RESULT_OK){
+                //getting file from external storage
+                selectedVideoUri = result.data?.data
+                showPostView()
+            }
+        }
         //getting profile userId from MainActivity.kt
         profileUserId = intent.getStringExtra("profile_user_id")!!
         // getting current userId from Firebase
