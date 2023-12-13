1b0df663ad9ab0b4bf47ea1da6816f8680844254
coder <soumyodipsutra@gmail.com>
2023-12-13 22:47:25 +0530

    build permission for pick photo

--- app/src/main/java/com/example/tiktok/ProfileActivity.kt
+++ app/src/main/java/com/example/tiktok/ProfileActivity.kt
@@ -1,11 +1,15 @@
 package com.example.tiktok
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.tiktok.databinding.ActivityProfileBinding
@@ -54,7 +58,19 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     fun checkPermissionAndPickPhoto(){
-
+        var readExternalVideo : String=""
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            readExternalVideo = android.Manifest.permission.READ_MEDIA_VIDEO
+        }
+        else{
+            readExternalVideo = android.Manifest.permission.READ_EXTERNAL_STORAGE
+        }
+        if(ContextCompat.checkSelfPermission(this,readExternalVideo) == PackageManager.PERMISSION_GRANTED){
+            openVideoPicker()
+        }
+        else{
+            ActivityCompat.requestPermissions(this, arrayOf(readExternalVideo),100)
+        }
     }
 
     fun logout(){
