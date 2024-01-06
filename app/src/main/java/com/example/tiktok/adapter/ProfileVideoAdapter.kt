20e5fc10403aadd14945a08f36030d175751f1db
coder <soumyodipsutra@gmail.com>
2024-01-06 14:50:48 +0530

    making adapter.

--- app/src/main/java/com/example/tiktok/adapter/ProfileVideoAdapter.kt
+++ app/src/main/java/com/example/tiktok/adapter/ProfileVideoAdapter.kt
@@ -1,4 +1,11 @@
 package com.example.tiktok.adapter
 
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tiktok.databinding.ProfileVideoItemRowBinding
+
 class ProfileVideoAdapter {
+
+    inner class VideoViewHolder(private val binding: ProfileVideoItemRowBinding) : RecyclerView.ViewHolder(binding.root){
+
+    }
 }
