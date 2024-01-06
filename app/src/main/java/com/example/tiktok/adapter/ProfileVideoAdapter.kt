297efd5cf7bf370bab787922cc9a03fa0c80bb81
coder <soumyodipsutra@gmail.com>
2024-01-06 14:58:39 +0530

    making adapter.

--- app/src/main/java/com/example/tiktok/adapter/ProfileVideoAdapter.kt
+++ app/src/main/java/com/example/tiktok/adapter/ProfileVideoAdapter.kt
@@ -1,11 +1,35 @@
 package com.example.tiktok.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.tiktok.databinding.ProfileVideoItemRowBinding
+import com.example.tiktok.model.VideoModel
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 
-class ProfileVideoAdapter {
+class ProfileVideoAdapter(
+    options: FirestoreRecyclerOptions<VideoModel>
+) : FirestoreRecyclerAdapter<VideoModel, ProfileVideoAdapter.VideoViewHolder>(options) {
 
     inner class VideoViewHolder(private val binding: ProfileVideoItemRowBinding) : RecyclerView.ViewHolder(binding.root){
 
+
+        fun bind(video: VideoModel){
+            Glide.with(binding.thumbnailImageView)
+                .load(video.url)
+                .into(binding.thumbnailImageView)
+        }
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
+        val binding = ProfileVideoItemRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return VideoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: VideoViewHolder, position: Int, model: VideoModel) {
+        TODO("Not yet implemented")
+    }
+
 }
