05c1f1aebcb18daff1cf1182c2e05ecb2c47d4d3
coder <soumyodipsutra@gmail.com>
2024-01-12 22:54:10 +0530

    made single videoplayer activity.

---
+++ app/src/main/java/com/example/tiktok/SingleVideoPlayerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tiktok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SingleVideoPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_video_player)
+    }
+}
