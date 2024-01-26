43cddc75ccd39f460004609b96d883f1fc7533f0
coder <soumyodipsutra@gmail.com>
2024-01-26 21:37:47 +0530

    making SingleVideoPlayerActivity.kt

--- app/src/main/java/com/example/tiktok/SingleVideoPlayerActivity.kt
+++ app/src/main/java/com/example/tiktok/SingleVideoPlayerActivity.kt
@@ -2,10 +2,17 @@ package com.example.tiktok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiktok.databinding.ActivitySingleVideoPlayerBinding
 
 class SingleVideoPlayerActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySingleVideoPlayerBinding
+    lateinit var videoId: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_single_video_player)
+        binding = ActivitySingleVideoPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
