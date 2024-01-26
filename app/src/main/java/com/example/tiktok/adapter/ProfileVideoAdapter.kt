b3988741d3b3a91df9bc8c12dea5244d812f0080
coder <soumyodipsutra@gmail.com>
2024-01-26 21:20:23 +0530

    making some changes

--- app/src/main/java/com/example/tiktok/adapter/ProfileVideoAdapter.kt
+++ app/src/main/java/com/example/tiktok/adapter/ProfileVideoAdapter.kt
@@ -12,32 +12,7 @@ import com.example.tiktok.model.VideoModel
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 
-class ProfileVideoAdapter(
-    options: FirestoreRecyclerOptions<ViewModel>
-) : FirestoreRecyclerAdapter<VideoModel, ProfileVideoAdapter.VideoViewHolder>(options) {
+class ProfileVideoAdapter{
 
-    inner class VideoViewHolder(private val binding: ProfileVideoItemRowBinding) : RecyclerView.ViewHolder(binding.root){
-
-
-        fun bind(video: VideoModel){
-            Glide.with(binding.thumbnailImageView)
-                .load(video.url)
-                .into(binding.thumbnailImageView)
-            binding.thumbnailImageView.setOnClickListener {
-                val intent = Intent(binding.thumbnailImageView.context,SingleVideoPlayerActivity::class.java)
-                intent.putExtra("videoId",video.videoId)
-                binding.thumbnailImageView.context.startActivity(intent)
-            }
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
-        val binding = ProfileVideoItemRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return VideoViewHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: VideoViewHolder, position: Int, model: VideoModel) {
-        holder.bind(model)
-    }
-
+    inner class VideoViewHolder(private val binding: ProfileVideoItemRowBinding) : RecyclerView.ViewHolder
 }
